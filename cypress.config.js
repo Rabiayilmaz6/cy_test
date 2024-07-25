b5e7e73b4630fe16c014b9d6e84fe6806907467a
RabiaYilmaz6 <21060999@stu.omu.edu.tr>
2024-07-25 16:23:47 +0300

    first

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,15 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'http://localhost:3000',
+    viewportWidth: 1000,
+    viewportHeight: 660,
+    defaultCommandTimeout: 10000,
+    pageLoadTimeout: 60000,
+    video: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
-    },
-  },
-});
+    }
+  }
+})
